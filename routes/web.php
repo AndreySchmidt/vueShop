de9c7e01facbf00b3bd5547b6d0351807da6b05d
AndreySchmidt <ringnull@gmail.com>
2022-08-04 17:25:42 +0300

    admin lte index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/admin', \App\Http\Controllers\Admin\IndexController::class)->name('admin.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
