483d78898235e5fa474e30749fd5d34839718640
AndreySchmidt <ringnull@gmail.com>
2022-08-06 15:30:45 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
