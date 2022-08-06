c2faba2b4bf2a32499ae0b72d80bf961c1ac82aa
AndreySchmidt <ringnull@gmail.com>
2022-08-06 15:28:27 +0300

    переименовал, потому что работать будет через фронт

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', \App\Http\Controllers\Admin\IndexController::class)->name('admin.index');
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
 Route::get('/', function () {
     return view('welcome');
